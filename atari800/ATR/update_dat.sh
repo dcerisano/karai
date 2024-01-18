70c804d746158c3a49c3eac7b74963e46482b0a7
standard3d <standard3d@standard3d.com>
2024-01-18 16:42:13 -0500

    init

--- atari800/ATR/update_dat.sh
+++ atari800/ATR/update_dat.sh
@@ -1,13 +1,16 @@
+HOME=~/git/atariVT/atari800/ATR
+cd $HOME
 while true
 do
-  echo "HOST: $(hostname)"                                                                                    > ~/atari800/ATR/RAW.DAT
-  echo "  OS: $(lsb_release -ds)"                                                                            >> ~/atari800/ATR/RAW.DAT
-  echo "  UP: $(uptime -p)"                                                                                  >> ~/atari800/ATR/RAW.DAT
-  echo "TIME: $(date)"                                                                                       >> ~/atari800/ATR/RAW.DAT
-  top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "%CPU: TOTAL " 100 - $1 "%"}' >> ~/atari800/ATR/RAW.DAT
-  top -b -n 5 | head -n 12  | tail -n 5 | cut  -c51-56,72-100                                                >> ~/atari800/ATR/RAW.DAT
-  tail -n 5 /var/log/apache2/access.log | cut -c2-22,30-80 | cut -d' ' -f1,2                                 >> ~/atari800/ATR/RAW.DAT
-  awk '{printf "%-37s\n",$0}' RAW.DAT                                                                         > ~/atari800/ATR/FIXED.DAT 
-  tr '\n' '\233' < ~/atari800/ATR/FIXED.DAT                                                                   > ~/atari800/ATR/FRAME.DAT 
+  echo "HOST: $(hostname)"                                                                                    > RAW.DAT
+  echo "  OS: $(lsb_release -ds)"                                                                            >> RAW.DAT
+  echo "  UP: $(uptime -p)"                                                                                  >> RAW.DAT
+  echo "TIME: $(date)"                                                                                       >> RAW.DAT
+  top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "%CPU: TOTAL " 100 - $1 "%"}' >> RAW.DAT
+  top -b -n 5 | head -n 12  | tail -n 5 | cut  -c51-56,72-100                                                >> RAW.DAT
+  tail -n 5 /var/log/apache2/access.log | cut -c2-22,30-80 | cut -d' ' -f1,2                                 >> RAW.DAT
+  awk '{printf "%-37s\n",$0}' RAW.DAT                                                                         > FIXED.DAT 
+  tr '\n' '\233' < FIXED.DAT                                                                                  > FRAME.DAT 
   sleep 1
+  echo TICK
 done
