42b76146e22d1f5681475114884030cd82d1e87b
praxis <dcerisano@gmail.com>
2024-01-20 18:24:31 -0500

    syslog

--- atari800/ATR/update_dat.sh
+++ atari800/ATR/update_dat.sh
@@ -6,10 +6,13 @@ do
   echo "  OS: $(lsb_release -ds)"                                                                            >> RAW.DAT
   echo "  UP: $(uptime -p)"                                                                                  >> RAW.DAT
   echo "TIME: $(date)"                                                                                       >> RAW.DAT
-  top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "%CPU: TOTAL " 100 - $1 "%"}' >> RAW.DAT
-  top -b -n 5 | head -n 12  | tail -n 5 | cut  -c51-56,72-100                                                >> RAW.DAT
-  tail -n 5 /var/log/apache2/access.log | cut -c2-22,30-80 | cut -d' ' -f1,2                                 >> RAW.DAT
-  awk '{printf "%-37s\n",$0}' RAW.DAT                                                                         > FIXED.DAT 
-  tr '\n' '\233' < FIXED.DAT                                                                                  > FRAME.DAT 
+  echo                                                                                                       >> RAW.DAT
+  top -b -n 1 | head -n 17  | tail -n 11 | cut  -c51-56,72-100                                               >> RAW.DAT
+  top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "TOTAL " 100 - $1 "%"}'      >> RAW.DAT
+  echo                                                                                                       >> RAW.DAT
+  echo "LOGS:"                                                                                               >> RAW.DAT
+  tail -n 15 /var/log/syslog | cut -c12-19,40-80 | cut -c1-37                                                >> RAW.DAT
+  # tail -n 5 /var/log/apache2/access.log | cut -c2-22,30-80 | cut -d' ' -f1,2                               >> RAW.DAT
+  awk '{printf "%-37s\n",$0}' RAW.DAT | tr '\n' '\233'                                                        > FRAME.DAT
   sleep 1
 done
