97a5bfd83e7b43bf918818907115ff045cfb0cb7
praxis <dcerisano@gmail.com>
2024-01-22 04:12:53 -0500

    flip

--- atari800/ATR/atarivt.sh
+++ atari800/ATR/atarivt.sh
@@ -8,11 +8,11 @@ do
   echo "TIME: $(date)"                                                                                       >> RAW.DAT
   echo                                                                                                       >> RAW.DAT
   top -b -n 1 | head -n 17  | tail -n 11 | cut  -c51-56,72-106                                               >> RAW.DAT
-  top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "TOTAL " 100 - $1 "%"}'      >> RAW.DAT
+  top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "TOTAL " 100 - $1 "%"}'       >> RAW.DAT
   echo                                                                                                       >> RAW.DAT
   echo "LOGS:"                                                                                               >> RAW.DAT
-  tail -n 15 /var/log/syslog | cut -c1-40                                                >> RAW.DAT
+  tail -n 15 /var/log/syslog | cut -c1-40                                                                    >> RAW.DAT
   # tail -n 5 /var/log/apache2/access.log | cut -c2-22,30-80 | cut -d' ' -f1,2                               >> RAW.DAT
-  awk '{printf "%-40s\n",$0}' RAW.DAT | tr '\n' '\233'                                                        > FRAME.DAT
+  awk '{printf "%-45s\n",$0}' RAW.DAT | tr '\n' '\233'                                                       > FRAME.DAT
   sleep 1
 done
