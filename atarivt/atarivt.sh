1175e62775f44ac2dd304a9bafab4e26c3e107a5
dcerisano <dcerisano@gmail.com>
2025-12-18 00:32:26 -0500

    Update atarivt script and autorun.atr, add data files

--- atarivt/atarivt.sh
+++ atarivt/atarivt.sh
@@ -1,5 +1,11 @@
+killall -9 /usr/bin/sh -o 1s || true
+killall -9  RespeQt || true
+
+/home/praxis/respectqt/RespeQt-r5.4.0_debian_9_x86_64/RespeQt --platform offscreen &
+
 HOME=~/git/karai/atarivt
 cd $HOME
+rm *.DAT
 while true
 do
   echo "HOST: $(hostname) $(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')"                                                                                    > RAW.DAT
@@ -14,6 +20,6 @@ do
   tail -n 4 /var/log/syslog | cut -c1-40                                                                     >> RAW.DAT
   # tail -n 5 /var/log/apache2/access.log | cut -c2-22,30-80 | cut -d' ' -f1,2                               >> RAW.DAT
   awk '{printf "%-40s\n",$0}' RAW.DAT | tr '\n' '\233' | tr '[\040-\137]' '[\000-\077]'                       > FRAME.DAT
-  sleep 0.5
+ # sleep 0.5
 
 done
